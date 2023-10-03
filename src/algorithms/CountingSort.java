726b0d3c06a627c05d5e46eed632e80159dbf29a
Guilherme Moreira <51029818+GuilhermeMoreira-Bold@users.noreply.github.com>
2023-10-03 13:49:04 -0300

    add the binary converter and the decimal converter

---
+++ src/algorithms/CountingSort.java
@@ -0,0 +1,14 @@
+package src.algorithms;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class CountingSort implements SortingAlgorithm {
+
+    @Override
+    public LinkedList<Integer> sort(LinkedList<Integer> lista) {
+
+
+        return null;
+    }
+}
--- src/algorithms/RadixSort.java
+++ src/algorithms/RadixSort.java
@@ -1,11 +1,19 @@
 package src.algorithms;
 
 import javax.swing.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.function.IntBinaryOperator;
 
 public class RadixSort {
+
+    LinkedList<String> positive = new LinkedList<>();
+    LinkedList<String> negative = new LinkedList<>();
+
+
     private static int getMax(int arr[]) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -94,47 +102,73 @@ public class RadixSort {
 
 
 //        int number2 = Integer.parseInt(binaryStringNegative,2);
+        System.out.println(decimalToBinaryNegative(decimalToBinary(999999999999999999L)));
+        System.out.println( decimalToBinary( 999999999999999999L));
+        System.out.println(binaryToDecimal(decimalToBinary(999999999999999999L)));
+        System.out.println(binaryToDecimal(decimalToBinaryNegative(decimalToBinary(999999999999999999L))));
 
-
-
-        System.out.println(decimalToBinaryNegative(decimalToBinary(3)));
-        System.out.println( decimalToBinary( 4));
-        System.out.println(decimalToBinaryNegative(decimalToBinary(2)));
-        System.out.println( decimalToBinary( 1));
-        System.out.println( decimalToBinary( 5));
-        System.out.println( decimalToBinary( 2));
     }
 
-    public static String decimalToBinary(int number){
+    public static String decimalToBinary(long number){
         StringBuilder builder = new StringBuilder();
         while (number != 0) {
             builder.append(number % 2);
             number /= 2;
         }
-        for(int i = 0; builder.length() < 32; i ++){
+        for(int i = 0; builder.length() < 64; i ++){
             builder.append("0");
         }
         return  builder.reverse().toString();
     }
 
     public static String decimalToBinaryNegative(String number){
-        StringBuilder builder = new StringBuilder(number);
+        number = flipBinary(number);
+        number = plusOneBinary(number);
+        return number;
+    }
+    public static String plusOneBinary(String binaryNumber){
         char zero = '0';
         char one = '1';
-        for(int i = 0; i < number.length(); i++){
-            if(builder.charAt(i) == '0'){
-                builder.setCharAt(i, one);
-                continue;
-            }
-                builder.setCharAt(i, zero);
-        }
-        for(int j = number.length() -1; j >= 0; j--){
-            if(builder.charAt(j) == '0'){
+        StringBuilder builder = new StringBuilder(binaryNumber);
+        for(int j = binaryNumber.length() -1; j >= 0; j--){
+            if(binaryNumber.charAt(j) == '0'){
                 builder.setCharAt(j, one);
                 break;
             }
             builder.setCharAt(j, zero);
         }
         return builder.toString();
+    }
+    public static String flipBinary(String numberToFlip){
+        StringBuilder builder = new StringBuilder(numberToFlip);
+        char zero = '0';
+        char one = '1';
+        for(int i = 0; i < builder.length(); i++){
+            if(builder.charAt(i) == '0'){
+                builder.setCharAt(i, one);
+                continue;
+            }
+            builder.setCharAt(i, zero);
+        }
+        return  builder.toString();
+    }
+
+    public static long binaryToDecimal(String number){
+
+        StringBuilder builder = new StringBuilder(number);
+        long decimalNumber = 0L;
+        if(builder.charAt(0) == '1'){
+           StringBuilder builder2 = new StringBuilder(plusOneBinary(flipBinary(builder.toString())));
+           builder2.reverse();
+            for(int i =  0; i < number.length(); i++){
+                decimalNumber += (builder2.charAt(i) - '0') * (long) Math.pow(2,i);
+            }
+            return decimalNumber * -1;
+        }
+        builder.reverse();
+        for(int i =  0; i < number.length(); i++){
+            decimalNumber += (builder.charAt(i) - '0') * (long) Math.pow(2,i);
+        }
+        return decimalNumber;
     }
 }
